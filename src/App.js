77db79efd3216fc182f4cf8985f543340b4f0393
MakarandPundlik <mmp2308@gmail.com>
2021-01-19 22:33:25 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+
 
 import { BrowserRouter, Switch , Route, Redirect} from 'react-router-dom';
 import './App.css';
-import Home from './components/Home';
-import Contact from './components/Contact';
-import About from './components/About';
-import Navbar from './components/Navbar';
+import Home from './Home';
+import Contact from './Contact';
+import About from './About';
+import Navbar from './Navbar';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      
       <BrowserRouter>
+      <Navbar/> {/*importtant*/}
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/about" component={About}/>
