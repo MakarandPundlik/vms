ee92986d57c02c5d7e02a90880cf5489352e1526
MakarandPundlik <mmp2308@gmail.com>
2021-01-19 17:59:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 
+import { BrowserRouter, Switch , Route, Redirect} from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/contact" component={Contact}/>
+        <Redirect to="/"/>
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact(props) {
+    return (
+        <div>
+            ContactPage
+        </div>
+    );
+}
+
+export default Contact;
